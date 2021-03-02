473429d08a9f493f5736af0e26449ae7c1a5b3a9
Jan Niklas Roesch <j.n.roesch@googlemail.com>
2021-03-03 00:29:55 +0100

    Initial package upload

---
+++ Runtime/TestFixture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace Packages.com.DeHagge.PrivateTestFramework.Runtime
+{
+    public class TestFixture
+    {
+        protected static T GetPrivateFieldValue<T>(object obj, string privateFieldName)
+        {
+            var field =
+                obj.GetType().GetField(privateFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            return (T) field?.GetValue(obj);
+        }
+
+        protected static void SetPrivateFieldValue<T>(object obj, string privateFieldName, T value)
+        {
+            var field =
+                obj.GetType().GetField(privateFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            field?.SetValue(obj, value);
+        }
+
+        protected static void InvokePrivateMethod(object obj, string methodName, params object[] methodParams)
+        {
+            var method = obj.GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic);
+            if (method == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            method.Invoke(obj, methodParams);
+        }
+        
+        protected static T InvokePrivateMethod<T>(object obj, string methodName, params object[] methodParams)
+        {
+            var method = obj.GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic);
+            if (method == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return (T) method.Invoke(obj, methodParams);
+        }
+    }
+}
---
+++ Tests/TestFixtureTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using Packages.com.DeHagge.PrivateTestFramework.Runtime;
+
+namespace Packages.com.DeHagge.PrivateTestFramework.Tests
+{
+    public class TestFixtureTests
+    {
+        private class TestObj
+        {
+            private string fieldString = "abc";
+            private int fieldInt = -1;
+
+            private void SetIntToZero()
+            {
+                fieldInt = 0;
+            }
+        }
+
+        public class GetPrivateFieldValue : TestFixture
+        {
+            [Test]
+            public void FieldDoesExist_ThenReturnValue()
+            {
+                var testObj = new TestObj();
+                var field = GetPrivateFieldValue<string>(testObj, "fieldString");
+                Assert.AreEqual("abc", field);
+            }
+            
+            [Test]
+            public void FieldDoesExistButOtherType_ThenThrowException()
+            {
+                var testObj = new TestObj();
+                Assert.Throws<InvalidCastException>(() => GetPrivateFieldValue<string>(testObj, "fieldInt"));
+            }
+            
+            [Test]
+            public void FieldDoesNotExist_ThenReturnNull()
+            {
+                var testObj = new TestObj();
+                Assert.IsNull(GetPrivateFieldValue<string>(testObj, "test"));
+            }
+        }
+    }
+}
